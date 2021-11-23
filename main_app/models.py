756de0bc221330578213c5f0740a06afadf34cbb
mattgefen <matt.gefen@gmail.com>
2021-11-23 08:38:49 -0700

    creating user structure and admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Poem(models.Model):
+  title = models.CharField(max_length=150)
+  author = models.CharField(max_length=100)
+  linecount = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Poem
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+def home(request):
+  return HttpResponse("Poetry is ordinary language raised to the Nth power.")
