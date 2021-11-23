40b8277e5c9890fb00e911a70cf64d7023e4ed67
mattgefen <matt.gefen@gmail.com>
2021-11-23 08:55:02 -0700

    adding lines array to poem model

--- main_app/models.py
+++ main_app/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Poem(models.Model):
   title = models.CharField(max_length=150)
   author = models.CharField(max_length=100)
+  lines = ArrayField(models.CharField(max_length=200))
   linecount = models.IntegerField()
   user = models.ForeignKey(User, on_delete=models.CASCADE)
