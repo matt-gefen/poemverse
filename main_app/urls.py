0c25160db9741889ec407a7443464d000568394c
mattgefen <matt.gefen@gmail.com>
2021-11-24 10:13:11 -0500

    index page and random poem under 30 lines

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.Home.as_view(), name='home'),
+  path('poems/', views.poems_index, name='poems_index'),
   path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,22 @@ from .models import Poem
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
+import requests
+import json
 
 # Create your views here.
 class Home(LoginView):
   template_name = 'home.html'
+
+def poems_index(request):
+  linecount = 0
+
+  while int(linecount) >= 30 or int(linecount) == 0:
+    response = requests.get('https://poetrydb.org/random').json()
+    response = response[0]
+    linecount = response['linecount']
+
+  return render(request, 'poems/index.html', {'poem':response} )
 
 def signup(request):
   error_message = ''
