f944e40f95812cd7f78532c8d24cd6d9ee627d36
mattgefen <matt.gefen@gmail.com>
2021-11-28 14:01:18 -0500

    background image styling

--- main_app/static/js/cards.js
+++ main_app/static/js/cards.js
@@ -1,20 +1,3 @@
-// const poems = document.querySelectorAll(".poem-text")
-// const showButtons = document.querySelectorAll('.show-button')
-
-// function hidePoem(poem) {
-//   console.log(poem)
-//   if (poem.hidden) {
-//     setTimeout(() => {poem.hidden = false}, 0)
-//   } else {
-//     setTimeout(() => {poem.hidden = true}, 0)
-//   }
-// } 
-// let counter = 0
-// for(button in showButtons) {
-//   let poem = poems[counter]
-//   button.addEventListener('click', hidePoem(poem))
-// }
-
 function hidePoem(name) {
   let poemdiv = document.getElementsByName(name)
   poemdiv = poemdiv[0]
