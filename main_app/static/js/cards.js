967fa79255623734bdb9816aed70537639dd058e
mattgefen <matt.gefen@gmail.com>
2021-11-30 19:22:18 -0700

    added hamburger menu w basic style

--- main_app/static/js/cards.js
+++ main_app/static/js/cards.js
@@ -11,3 +11,17 @@ function hidePoem(name) {
     poembtn.innerHTML = `<i class="fas fa-ellipsis-h"></i>`
   }
 }
+
+
+function hideNav() {
+  let hiddenNav = document.querySelector(".hidden-nav")
+  if (hiddenNav.hidden) {
+    hiddenNav.hidden = false
+  } else {
+    hiddenNav.hidden = true
+  }
+}
+
+let hamburger = document.querySelector(".hamburger")
+
+hamburger.addEventListener("click", hideNav)
